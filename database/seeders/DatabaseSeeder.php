d9b2159f35eb96b441d7351428a2b4acfd211f0e
iqbalatma <iqbalatma@gmail.com>
2022-11-20 01:06:59 +0700

    set up user table done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UnitSeeder::class,
+            RoleSeeder::class,
+            UserSeeder::class,
         ]);    
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                "id_number" => "6101071602990002",
+                "name" => "iqbal atma muliawan",
+                "email" => "iqbalatma@gmail.com",
+                "password" => Hash::make("admin"),
+                "address" => "selakau",
+                "phone" => "+62895351172040",
+                "role_id" => "1",
+            ]
+        ];
+
+        foreach ($data as $key => $item) {
+            User::create($item);
+        }
+    }
+}
