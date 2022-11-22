16c48f40094de15b7a14a5562d999f75d103a173
iqbalatma <iqbalatma@gmail.com>
2022-11-22 17:25:11 +0700

    logout done

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\AuthenticateRequest;
 use App\Services\AuthService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
@@ -29,13 +31,29 @@ class AuthController extends Controller
      * @param AuthService $service dependency injection
      * @param AuthenticateRequest $request dependency injection
      */
-    public function authenticate(AuthService $service, AuthenticateRequest $request)
+    public function authenticate(AuthService $service, AuthenticateRequest $request):RedirectResponse
     {
         if(Auth::attempt($request->validated())){
             return redirect()->intended(route("dashboard.index"));
         }
 
         return redirect()->back()->with("failed", "Username or password is invalid");
+    }
+
+
+    /**
+     * Description : use to logout account
+     * 
+     * @param Request $request
+     */
+    public function logout(Request $request):RedirectResponse
+    {
+        Auth::logout();
+ 
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+    
+        return redirect('/');
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -30,72 +30,76 @@ Route::controller(AuthController::class)
     ->group(function (){
         Route::get("/login", "login")->name("login");
         Route::post("/authenticate", "authenticate")->name("authenticate");
+        Route::post("/logout", "logout")->name("logout");
     });
 
 
-Route::controller(DashboardController::class)
-    ->name("dashboard.")
-    ->prefix("/dashboard")
+Route::middleware("auth")
     ->group(function (){
-        Route::get("/", "index")->name("index");
-    });
+        Route::controller(RestockController::class)
+            ->name("restock.")
+            ->prefix("/restock")
+            ->group(function (){
+                Route::get("/create", "create")->name("create");
+                Route::post("/", "store")->name("store");
+            });
 
-
-Route::controller(UnitController::class)
-    ->name("units.")
-    ->prefix("/units")
-    ->group(function (){
-        Route::get("/", "index")->name("index");
-        Route::get("/edit/{id}", "edit")->name("edit");
-        Route::get("/create", "create")->name("create");
-        Route::patch("/{id}", "update")->name("update");
-        Route::post("/", "store")->name("store");
-        Route::delete("/{id}", "destroy")->name("destroy");
-    });
-
-Route::controller(RoleController::class)
-    ->name("roles.")
-    ->prefix("/roles")
-    ->group(function (){
-        Route::get("/", "index")->name("index");
-    });
-
-Route::controller(CustomerController::class)
-    ->name("customers.")
-    ->prefix("/customers")
-    ->group(function (){
-        Route::get("/", "index")->name("index");
-        Route::get("/create", "create")->name("create");
-        Route::get("/edit/{id}", "edit")->name("edit");
-        Route::post("/", "store")->name("store");
-        Route::patch("/{id}", "update")->name("update");
-        Route::delete("/{id}", "destroy")->name("destroy");
-    });
-
-Route::controller(RollController::class)
-    ->name("rolls.")
-    ->prefix("/rolls")
-    ->group(function (){
-        Route::get("/", "index")->name("index");
-        Route::get("/create", "create")->name("create");
-        Route::post("/", "store")->name("store");
-        Route::get("/edit/{id}", "edit")->name("edit");
-        Route::patch("/{id}", "update")->name("update");
-    });
-
-Route::controller(RollTransactionController::class)
-    ->name("roll.transactions.")
-    ->prefix("/roll-transactions")
-    ->group(function (){
-        Route::get("/", "index")->name("index");
-        Route::get("/put-away", "putAway")->name("putAway");
-        Route::post("/put-away", "putAwayTransaction")->name("putAwayTransaction");
-    });
-
-Route::controller(RestockController::class)
-    ->name("restock.")
-    ->prefix("/restock")
-    ->group(function (){
-        Route::get("/create", "create")->name("create");
-        Route::post("/", "store")->name("store");
+        Route::controller(DashboardController::class)
+            ->name("dashboard.")
+            ->prefix("/dashboard")
+            ->group(function (){
+                Route::get("/", "index")->name("index");
+            });
+        
+        
+        Route::controller(UnitController::class)
+            ->name("units.")
+            ->prefix("/units")
+            ->group(function (){
+                Route::get("/", "index")->name("index");
+                Route::get("/edit/{id}", "edit")->name("edit");
+                Route::get("/create", "create")->name("create");
+                Route::patch("/{id}", "update")->name("update");
+                Route::post("/", "store")->name("store");
+                Route::delete("/{id}", "destroy")->name("destroy");
+            });
+        
+        Route::controller(RoleController::class)
+            ->name("roles.")
+            ->prefix("/roles")
+            ->group(function (){
+                Route::get("/", "index")->name("index");
+            });
+        
+        Route::controller(CustomerController::class)
+            ->name("customers.")
+            ->prefix("/customers")
+            ->group(function (){
+                Route::get("/", "index")->name("index");
+                Route::get("/create", "create")->name("create");
+                Route::get("/edit/{id}", "edit")->name("edit");
+                Route::post("/", "store")->name("store");
+                Route::patch("/{id}", "update")->name("update");
+                Route::delete("/{id}", "destroy")->name("destroy");
+            });
+        
+        Route::controller(RollController::class)
+            ->name("rolls.")
+            ->prefix("/rolls")
+            ->group(function (){
+                Route::get("/", "index")->name("index");
+                Route::get("/create", "create")->name("create");
+                Route::post("/", "store")->name("store");
+                Route::get("/edit/{id}", "edit")->name("edit");
+                Route::patch("/{id}", "update")->name("update");
+            });
+        
+        Route::controller(RollTransactionController::class)
+            ->name("roll.transactions.")
+            ->prefix("/roll-transactions")
+            ->group(function (){
+                Route::get("/", "index")->name("index");
+                Route::get("/put-away", "putAway")->name("putAway");
+                Route::post("/put-away", "putAwayTransaction")->name("putAwayTransaction");
+            });
     });
