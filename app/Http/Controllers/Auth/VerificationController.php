4afedd1c94a97baf9b6fab665f8b4b8e2bddb2ff
iqbalatma <iqbalatma@gmail.com>
2022-12-08 13:08:21 +0700

    change redirect on show verification view

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Mail\OrderShipped;
-use App\Models\User;
-use App\Notifications\WelcomeEmailNotification;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 
 class VerificationController extends Controller
 {
@@ -17,13 +13,12 @@ class VerificationController extends Controller
     public function show(Request $request)
     {
         return $request->user()->hasVerifiedEmail()
-                        ? redirect($this->redirectPath())
+                        ? redirect(route("dashboard.index"))
                         : view('auth.verification', [
                             'title' => __('Account Verification'),
                             'cardTitle' => __('Account Verification'),
                             'pageTitle' => __('Account Verification')
                         ]);
-
     }
 
 
