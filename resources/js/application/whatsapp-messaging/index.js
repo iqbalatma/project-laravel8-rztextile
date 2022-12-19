e0edf766fc74d0ece9a83bee71ff9e47262ce362
iqbalatma <iqbalatma@gmail.com>
2022-12-19 21:56:06 +0700

    route reorganize and role middleware

--- resources/js/application/whatsapp-messaging/index.js
+++ resources/js/application/whatsapp-messaging/index.js
@@ -1,25 +1,23 @@
 $(document).ready(function () {
-  function escape(htmlStr) {
-    return htmlStr.replace(/&/g, "&amp;")
-          .replace(/</g, "&lt;")
-          .replace(/>/g, "&gt;")
-          .replace(/"/g, "&quot;")
-          .replace(/'/g, "&#39;");        
- 
- }  
-  
-  $("#promotion").on("change", function () {
-        const promotionId = $(this).val();
+    function escape(htmlStr) {
+        return htmlStr
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#39;");
+    }
 
-          $.ajax({
+    $("#promotion").on("change", function () {
+        const promotionId = $(this).val();
+        $.ajax({
             method: "GET",
-            url : `/ajax/promotion-messages/${promotionId}`
-          })
+            url: `/ajax/promotion-messages/${promotionId}`,
+        })
             .done(function (response) {
-              $("#message").html((response.data.message));
-              $("#message-input").val((response.data.message));
+                $("#message").html(response.data.message);
+                $("#message-input").val(response.data.message);
             })
             .fail();
-
     });
 });
