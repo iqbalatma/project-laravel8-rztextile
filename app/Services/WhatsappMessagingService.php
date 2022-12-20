f9bcf5f0fadc8f3f24c23980e9d482c2a40a7d39
iqbalatma <iqbalatma@gmail.com>
2022-12-20 11:26:48 +0700

    add description on whatsapp messaging

--- app/Services/WhatsappMessagingService.php
+++ app/Services/WhatsappMessagingService.php
@@ -9,46 +9,47 @@ use App\Repositories\PromotionMessageRepository;
 
 class WhatsappMessagingService
 {
-  private const GET_ALL_PROMOTION_MESSAGE_COLUMN = [
-    AppData::TABLE_PROMOTION_MESSAGE . ".id",
-    AppData::TABLE_PROMOTION_MESSAGE . ".name",
-  ];
-
-  private const GET_CUSTOMER_BY_IDS_COLUMN = [
-    AppData::TABLE_USER . ".phone",
-  ];
-
-  public function getAllData(): array
-  {
-    return [
-      "title" => "Whatsapp Messaging",
-      "cardTitle" => "Whatsapp Messaging",
-      "customers" => (new CustomerRepository())->getAllDataCustomer(),
-      "promotionMessages" => (new PromotionMessageRepository())->getAllDataPromotionMessage(self::GET_ALL_PROMOTION_MESSAGE_COLUMN)
+    private const GET_ALL_PROMOTION_MESSAGE_COLUMN = [
+        AppData::TABLE_PROMOTION_MESSAGE . ".id",
+        AppData::TABLE_PROMOTION_MESSAGE . ".name",
     ];
-  }
 
-  public function sendMessage(array $requestedData)
-  {
-    $payloads = $this->getDataPayload($requestedData);
-    return WablasTrait::sendMessage($payloads);
-  }
+    private const GET_CUSTOMER_BY_IDS_COLUMN = [
+        AppData::TABLE_USER . ".phone",
+    ];
 
-  private function getDataPayload(array $requestedData): array
-  {
-    $customers = (new CustomerRepository())->getCustomerByIds($requestedData["customer"], self::GET_CUSTOMER_BY_IDS_COLUMN);
+    public function getAllData(): array
+    {
+        return [
+            "title"             => "Whatsapp Messaging",
+            "description"       => "Send promotion broadcast message to customer",
+            "cardTitle"         => "Whatsapp Messaging",
+            "customers"         => (new CustomerRepository())->getAllDataCustomer(),
+            "promotionMessages" => (new PromotionMessageRepository())->getAllDataPromotionMessage(self::GET_ALL_PROMOTION_MESSAGE_COLUMN)
+        ];
+    }
 
-    $message = preg_replace('/<strong>|<\/strong>/', '*', $requestedData["message"]);
-    $message = preg_replace('/&nbsp;/', '', $message);
-    $message = preg_replace('/<em>|<\/em>/', '_', $message);
-    $message = preg_replace('/<p>|<\/p>/', '', $message);
-    $payload = collect($customers)->map(function ($item) use ($message) {
+    public function sendMessage(array $requestedData)
+    {
+        $payloads = $this->getDataPayload($requestedData);
+        return WablasTrait::sendMessage($payloads);
+    }
 
-      return [
-        "phone" => preg_replace('/[^0-9]/', '', $item->phone),
-        "message" => $message
-      ];
-    })->toArray();
-    return ["data" => $payload];
-  }
+    private function getDataPayload(array $requestedData): array
+    {
+        $customers = (new CustomerRepository())->getCustomerByIds($requestedData["customer"], self::GET_CUSTOMER_BY_IDS_COLUMN);
+
+        $message = preg_replace('/<strong>|<\/strong>/', '*', $requestedData["message"]);
+        $message = preg_replace('/&nbsp;/', '', $message);
+        $message = preg_replace('/<em>|<\/em>/', '_', $message);
+        $message = preg_replace('/<p>|<\/p>/', '', $message);
+        $payload = collect($customers)->map(function ($item) use ($message) {
+
+            return [
+                "phone"   => preg_replace('/[^0-9]/', '', $item->phone),
+                "message" => $message
+            ];
+        })->toArray();
+        return ["data" => $payload];
+    }
 }
