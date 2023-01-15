18137247d9bd5c6fc0dc683d105989f6a3df12ca
iqbalatma <iqbalatma@gmail.com>
2023-01-15 23:14:36 +0700

    webpack change

--- public/js/customers/index.js
+++ public/js/customers/index.js
@@ -1,16 +1,1 @@
-$(document).ready(function () {
-    $(".btn-delete").on("click", function () {
-        var t = $(this).closest("form");
-        Swal.fire({
-            title: "Are you sure want to delete the unit ?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: !0,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!",
-        }).then(function (e) {
-            e.isConfirmed && t.submit();
-        });
-    });
-});
+$(document).ready((function(){$(".btn-delete").on("click",(function(){var t=$(this).closest("form");Swal.fire({title:"Are you sure want to delete the unit ?",text:"You won't be able to revert this!",icon:"warning",showCancelButton:!0,confirmButtonColor:"#3085d6",cancelButtonColor:"#d33",confirmButtonText:"Yes, delete it!"}).then((function(e){e.isConfirmed&&t.submit()}))}))}));
