e10c4220300c618495c432757e29e269bcaa46d7
iqbalatma <iqbalatma@gmail.com>
2023-01-19 14:49:00 +0700

    shopping transaction done

--- app/Http/Controllers/Transactions/ShoppingController.php
+++ app/Http/Controllers/Transactions/ShoppingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Transactions;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Shopping\PurchaseRequest;
-use App\Services\ShoppingService;
+use App\Services\Transactions\ShoppingService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
--- app/Services/Transactions/ShoppingService.php
+++ app/Services/Transactions/ShoppingService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Transactions;
 
 use App\AppData;
 use App\Models\RollTransaction;
